5d691ff4a7670b8c5c3c530f6eb43e0016d766bf
Davidgu21 <dagu21@student.bth.se>
2022-04-25 15:40:47 +0200

    Follow_multiple_paths

--- Project_G19/Follow_path.py
+++ Project_G19/Follow_path.py
@@ -10,3 +10,53 @@ def obstical_check(drivebase, ultra_sens, ev3):
     if ultra_sens.distance() < 200:
         drivebase.drive(0, 0)
         ev3.speaker.say("Obstical detected")
+
+
+
+from asyncio.windows_events import NULL
+import sys
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+import lift
+
+
+
+def multiple_paths(drivebase, ultra_sens, ev3, left_sens):
+
+    DISTANCE_THRESHOLD = 140
+    REFLECTION_THRESHOLD = ColorSensor.reflection()
+    #Gets the first reflection it sees, put on circle line
+    DEFAULT_REFLECTION = ColorSensor.reflection()
+    DRIVE_SPEED = 50
+    TURN_SPEED = 40
+    desired_color = 1
+
+    wait(100)
+    ev3.speaker.beep()
+    # Revise - infinite while loops are *illegal*
+    while True:
+        if ultra_sens.distance() > DISTANCE_THRESHOLD:
+            if left_sens.color() == desired_color and left_sens.reflection != DEFAULT_REFLECTION:
+                REFLECTION_THRESHOLD = left_sens.reflection()
+            elif left_sens.reflection() < REFLECTION_THRESHOLD:
+                drivebase.drive(-DRIVE_SPEED, TURN_SPEED)
+            else:
+                drivebase.drive(-DRIVE_SPEED, -TURN_SPEED)
+            # if left_sens.color() == COLOR_THRESHOLD:
+            #     drivebase.drive(DRIVE_SPEED, TURN_SPEED)
+            # else:
+            #     drivebase.drive(DRIVE_SPEED, -TURN_SPEED)
+
+
+    # lift functionality
+    lift.lift(drivebase, lift_motor, 0)
+
+    return 0
+
+
