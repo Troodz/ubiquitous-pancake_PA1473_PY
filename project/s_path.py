16d37fea382b4f01a9fdc58578f09e79a37e8159
miranismail <miim21@student.bth.se>
2022-04-25 14:47:11 +0200

    generalised version

--- project/s_path.py
+++ project/s_path.py
@@ -1,40 +1,13 @@
 import "main.py"
-#black ==(0,0,0)
-def follow_path(text):
-    if text == "red":
-        color = (102,0,0)
-        color_now = color_sensor.measure()
-        while color_now != color 
-            go_robot_go()
-        go_to_color()
-    elif text =="green":
-        color = (0,102,0)
-        color_now = color_sensor.measure()
-        while color_now != color 
-            go_robot_go()
-        go_to_color()
-    elif text =="blue":
-        color = (1,0,102)
-        color_now = color_sensor.measure()
-        while color_now != color 
-            go_robot_go()
-        go_to_color()
-    elif text =="poo_green":
-        color = (101,102,0)
-        color_now = color_sensor.measure()
-        while color_now != color 
-            go_robot_go()
-        go_to_color()
-    elif text =="purple":
-        color = (102,0,102)
-        color_now = color_sensor.measure()
-        while color_now != color 
-            go_robot_go()
-        go_to_color()
-    elif text =="brown":
-        color = (91,43,83)
-        color_now = color_sensor.measure()
-        while color_now != color 
-            go_robot_go()
-        go_to_color()
-    return 0
+
+def follow_path(color_code):
+    go_robot_go()
+    while ColorSensor.color() == color_code:
+        stop()
+        drivebase.drive(100,0)
+        wait(100)
+        stop()
+        run_run_right()
+        go_robot_go()
+        while ColorSensor.color() == 1:
+            stop()
