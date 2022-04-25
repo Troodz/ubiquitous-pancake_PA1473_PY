ca3761102b7e5bfa5af1d4dd27956832eb29df76
miranismail <miim21@student.bth.se>
2022-04-25 14:22:40 +0200

    psuedocode v2

--- project/s_path.py
+++ project/s_path.py
@@ -1,23 +1,40 @@
-Import "main.py"
-
+import "main.py"
+#black ==(0,0,0)
 def follow_path(text):
     if text == "red":
-        color = (0,0,0)
+        color = (102,0,0)
         color_now = color_sensor.measure()
         while color_now != color 
             go_robot_go()
         go_to_color()
     elif text =="green":
-        color = (0,0,0)
+        color = (0,102,0)
         color_now = color_sensor.measure()
         while color_now != color 
             go_robot_go()
         go_to_color()
-    else:
-        color = (0,0,0)
+    elif text =="blue":
+        color = (1,0,102)
         color_now = color_sensor.measure()
         while color_now != color 
             go_robot_go()
         go_to_color()
-
+    elif text =="poo_green":
+        color = (101,102,0)
+        color_now = color_sensor.measure()
+        while color_now != color 
+            go_robot_go()
+        go_to_color()
+    elif text =="purple":
+        color = (102,0,102)
+        color_now = color_sensor.measure()
+        while color_now != color 
+            go_robot_go()
+        go_to_color()
+    elif text =="brown":
+        color = (91,43,83)
+        color_now = color_sensor.measure()
+        while color_now != color 
+            go_robot_go()
+        go_to_color()
     return 0
