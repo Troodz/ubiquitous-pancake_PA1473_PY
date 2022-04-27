ecb745d514abd05fe825fd04ed4a544e113534ba
simlju0519 <simonljung0519@gmail.com>
2022-04-27 13:41:18 +0200

    Test_files-folder updt

--- Project_G19/lift.py
+++ Project_G19/lift.py
@@ -6,29 +6,16 @@ from pybricks.robotics import DriveBase
 
 
 def lift_fork(lift_motor):
-    lift_motor.run_angle(10, 180)
+    lift_motor.run_angle(100, 60)
 
 
 def lower_lift_fork(lift_motor):
-    lift_motor.run_angle(10, -180)
+    lift_motor.run_angle(100, 60)
 
 
-def check_pallet(lift_motor):
-    if not has_pallet():
+def check_pallet(lift_motor, touch_sensor):
+    if not has_pallet(touch_sensor):
         lower_lift_fork(lift_motor)
-
-
-def lift_old(drive_base: DriveBase, lift_motor, touch_sensor: TouchSensor, height: int = 0):
-    drive_base.drive(100, 0)
-    time = StopWatch()
-    time.reset()
-    while not has_pallet(touch_sensor) and time.time() < 1000:
-        pass
-    drive_base.drive(0, 0)
-    wait(500)
-    lift_fork(lift_motor)
-    wait(500)
-    drive_base.straight(-100)
 
 
 def has_pallet(touch_sensor: TouchSensor):
@@ -36,19 +23,19 @@ def has_pallet(touch_sensor: TouchSensor):
 
 
 def lift(drive_base: DriveBase, lift_motor: Motor, touch_sensor: TouchSensor, height: int = 0):
-    drive_base.drive(100, 0)
+    drive_base.drive(-100, 0)
     time = StopWatch()
     time.reset()
-    while not has_pallet(touch_sensor) and time.time() < 1000:
+    while not has_pallet(touch_sensor) and time.time() < 4000:
         pass
     backing_time = time.time()
     if has_pallet(touch_sensor):
         drive_base.drive(0, 0)
         wait(500)
         lift_fork(lift_motor)
-        check_pallet(lift_motor) # kontrollerar om pallen fortfarande är kvar
+        check_pallet(lift_motor, touch_sensor) # kontrollerar om pallen fortfarande är kvar
         wait(500)
-    drive_base.drive(-100, 0)
+    drive_base.drive(100, 0)
     time = StopWatch()
     time.reset()
     while time.time() < backing_time: #Backar så långt som den körde fram
