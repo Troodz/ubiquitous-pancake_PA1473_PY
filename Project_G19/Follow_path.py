4c7b796936a5fe2cd466d10441693662f34d6911
MiranIsmail <90042468+MiranIsmail@users.noreply.github.com>
2022-04-27 15:21:28 +0200

    Update Follow_path.py
    New and updated multiple_path

--- Project_G19/Follow_path.py
+++ Project_G19/Follow_path.py
@@ -19,32 +19,18 @@ def obstical_ahead(drivebase, ultra_sens, ev3):
 def multiple_paths(drivebase, ultra_sens, ev3, left_sens):
 
     reflection_threshold = left_sens.reflection()
-    #Gets the first reflection it sees, put on circle line
     DEFAULT_REFLECTION = left_sens.reflection()
-    desired_color = "Color.RED"
-
     wait(100)
     ev3.speaker.beep()
-    # Revise - infinite while loops are *illegal*
-
-
-    while True:
-        if left_sens.color() == desired_color and left_sens.reflection != DEFAULT_REFLECTION:
-            reflection_threshold = left_sens.reflection()
-        
-        print("follows path")
-        print(reflection_threshold)
+    checker=True
+    follow_path(drivebase, left_sens, reflection_threshold)
+    while checker:
         print(left_sens.color())
-        follow_straight_path(drivebase, left_sens, reflection_threshold)
-        # if left_sens.color() == COLOR_THRESHOLD:
-        #     drivebase.drive(DRIVE_SPEED, TURN_SPEED)
-        # else:
-        #     drivebase.drive(DRIVE_SPEED, -TURN_SPEED)
+        if left_sens.color() == Color.RED and left_sens.reflection != DEFAULT_REFLECTION:
+            reflection_threshold = left_sens.reflection()
+            checker=False
+    print("follows path", reflection_threshold,left_sens.color())
+    follow_path(drivebase, left_sens, reflection_threshold)
 
-
-    # lift functionality
-    lift.lift(drivebase, lift_motor, 0)
-
-    return 0
 
 
