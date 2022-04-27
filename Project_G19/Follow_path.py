1b3fda170fb1a38f6f502895f58e3b0a8694dc5d
Davidgu21 <dagu21@student.bth.se>
2022-04-27 15:29:50 +0200

    quickfix

--- Project_G19/Follow_path.py
+++ Project_G19/Follow_path.py
@@ -2,6 +2,7 @@
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.parameters import Port, Stop, Direction, Button, Color
 
 def follow_straight_path(drivebase, left_sens, reflection_threshold, desired_color):
     if left_sens.color() == desired_color:
@@ -23,14 +24,14 @@ def multiple_paths(drivebase, ultra_sens, ev3, left_sens):
     wait(100)
     ev3.speaker.beep()
     checker=True
-    follow_path(drivebase, left_sens, reflection_threshold)
+    follow_straight_path(drivebase, left_sens, reflection_threshold)
     while checker:
         print(left_sens.color())
         if left_sens.color() == Color.RED and left_sens.reflection != DEFAULT_REFLECTION:
             reflection_threshold = left_sens.reflection()
             checker=False
     print("follows path", reflection_threshold,left_sens.color())
-    follow_path(drivebase, left_sens, reflection_threshold)
+    follow_straight_path(drivebase, left_sens, reflection_threshold)
 
 
 
