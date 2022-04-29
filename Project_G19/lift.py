2d9efa74d590fbd476475d7003dde8e7bd0bbf66
ImBonkers <1349651+ImBonkers@users.noreply.github.com>
2022-04-29 12:37:55 +0200

    lift update

--- Project_G19/lift.py
+++ Project_G19/lift.py
@@ -24,28 +24,31 @@ def has_pallet(touch_sensor: TouchSensor):
 
 
 def lift(drive_base: DriveBase, lift_motor: Motor, touch_sensor: TouchSensor, height: int = 0) -> bool:
-    drive_base.drive(-100, 0)
+    drive_base.drive(100, 0)
     time = StopWatch()
     time.reset()
 
+    # Move forward until a pallet is detected or the time has surpassed some time.
     while not has_pallet(touch_sensor) and time.time() < 4000:
-        pass
-    backing_time = time.time()
+        backing_time = time.time()
     drive_base.drive(0, 0)
 
+    # if the time it took to find the pallet is longer than some time then no pallet has been found.
     if backing_time >= 4000:
         return False
 
+    # otherwise if the pallet has been found then lift the fork.
     if has_pallet(touch_sensor):
         wait(500)
         lift_fork(lift_motor)
 
-    drive_base.drive(-100, 0)
     time.reset()
-    drive_base.drive(100, 0)
-    while time.time() < (backing_time-500): #Backar så långt som den körde fram
+    drive_base.drive(-100, 0)
+    while time.time() < backing_time: #Backar så långt som den körde fram
         pass
     drive_base.drive(0, 0)
+
+    return True
 
 
 if __name__ == '__main__':
