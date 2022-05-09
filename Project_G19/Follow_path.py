0efb18c057a0d0e40a36d808019404147dbea80e
WavenXD <artursbiten@gmail.com>
2022-05-09 15:31:58 +0200

    Update Follow_path.py

--- Project_G19/Follow_path.py
+++ Project_G19/Follow_path.py
@@ -4,10 +4,9 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 
-
 def follow_straight_path(drivebase, left_sens, desired_color, ev3):
     ev3.light.on(desired_color)
-    ev3.screen.draw_text(50, 50, str(desired_color))
+    ev3.screen.draw_text(50,50,str(desired_color))
     if left_sens.color() == desired_color:
         drivebase.drive(60, -40)
     else:
@@ -23,6 +22,7 @@ def obstacle_ahead(drivebase, ultra_sens, ev3):
 
 
 def find_desired_path(ev3, drivebase: DriveBase, desired_color: Color, left_sens: ColorSensor):
+
     drivebase.straight(100)
     # reverse and turn, try to keep the sensor from moving too much.
     drivebase.drive(-70, 70)
@@ -32,3 +32,5 @@ def find_desired_path(ev3, drivebase: DriveBase, desired_color: Color, left_sens
     drivebase.drive(0, 0)
     ev3.screen.draw_text(50, 50, "Successfully entered a new area")
     return desired_color
+
+
